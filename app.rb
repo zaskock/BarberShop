096dde9c8bf16dad93432a0f6bb10f92f1875170
Valera <bomzadua@yomail.info>
2017-02-28 01:30:57 +0200

    fetching all records from 'barbers' table to a global var.

--- app.rb
+++ app.rb
@@ -12,5 +12,6 @@ class Barber<ActiveRecord::Base
 end
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	@barbers = Barber.all
+	erb :index
 end
