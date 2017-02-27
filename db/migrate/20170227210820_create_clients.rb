5e18706739731e6271beed1ee7e73e4e1358bfcd
Valera <bomzadua@yomail.info>
2017-02-27 23:19:08 +0200

    migration added for 'client'

---
+++ db/migrate/20170227210820_create_clients.rb
@@ -0,0 +1,15 @@
+class CreateClients < ActiveRecord::Migration
+	def change
+
+		create_table :client do |t|
+		t.text :name
+		t.text :phone
+		t.text :datestamp
+		t.text :barber
+		t.text :color
+#creates fields 'created_at' & 'ipdated_at' in the table
+		t.timestamps
+	end
+
+	end
+end
